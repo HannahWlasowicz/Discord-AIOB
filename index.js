ad211937bf3ed4b03552f5da5933e105480fa123
Hannah <hannahwl@buffalo.edu>
2021-08-26 09:17:25 -0400

    initial setup of file

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Bot is online :)')
+});
+
+//Put in hidden key
+client.login('');
