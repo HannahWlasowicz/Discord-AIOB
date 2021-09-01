bccfedccbc8fb8e0264ab708fea8037cd11b3f2c
Hannah <hannahwl@buffalo.edu>
2021-09-01 19:54:23 -0400

    setting up basic commands

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-const client = new Discord.Client();
+const prefix = '-';
 
-client.once('ready', () => {
-    console.log('Bot is online :)')
-});
 
-//Put in hidden key
-client.login('');
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+  
+client.on('messageCreate', message =>{
+  if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(message.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if(command === 'ping'){
+    message.channel.send('pong');
+  }
+  else{
+    message.channel.send("What??")
+  }
+
+})
+  client.login('ODgyNzY0NzAyMjczMzg4NTU0.YTAIsQ.K8SNZWttvugJMf4IXRzOItfEZWQ');
