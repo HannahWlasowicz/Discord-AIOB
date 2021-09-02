f2847ddab1c04543230e8bb549899be4aa564233
Hannah <hannahwl@buffalo.edu>
2021-09-01 20:12:25 -0400

    commands are now through file system

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const prefix = '-';
+
+// Get's commands from folder
+const fs = require('fs');
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
+}
 
 
 client.on('ready', () => {
@@ -11,15 +23,15 @@ client.on('ready', () => {
 client.on('messageCreate', message =>{
   if(!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(message.length).split(/ +/);
+  const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
   if(command === 'ping'){
-    message.channel.send('pong');
+    client.commands.get('ping').execute(message, args);
   }
   else{
     message.channel.send("What??")
   }
 
 })
-  client.login('ODgyNzY0NzAyMjczMzg4NTU0.YTAIsQ.K8SNZWttvugJMf4IXRzOItfEZWQ');
+  client.login('');
