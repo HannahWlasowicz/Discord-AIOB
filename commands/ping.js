94f243dcffc60f80ebf849366b079d070e81de11
Hannah <hannahwl@buffalo.edu>
2021-09-04 22:06:06 -0400

    adds role to user when running command

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,12 @@ module.exports = {
     name: 'ping',
     description: "This is a ping command!",
     execute(message, args){
-        message.channel.send('pong!');
+        if(message.member.roles.cache.has('883895014688718918')){
+            message.channel.send('pong!');
+        }
+        else {
+            message.channel.send("Let's add that role to you ;)");
+            message.member.roles.add('883895014688718918').catch(console.error);
+        }
     }
 }
