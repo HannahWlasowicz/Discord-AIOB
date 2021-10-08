01bd2823fb7d2115055606295a010ee9cc5b1f2d
Hannah <hannahwl@buffalo.edu>
2021-10-08 13:35:51 -0400

    role added when a user joins

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
   
+client.on('guildMemberAdd', guildMember => {
+  let welcomeRole = guildMember.guild.roles.cache.find(role => role.name ==='Member');
+
+  guildMember.roles.add(welcomeRole);
+  guildMember.guild.channels.cache.get('883172952202424330').send(`Welcome <@${guildMember.user.id}> to our server! Make sure to checkout #rules channel`);
+});
+
 client.on('messageCreate', message =>{
   if(!message.content.startsWith(prefix) || message.author.bot) return;
 
